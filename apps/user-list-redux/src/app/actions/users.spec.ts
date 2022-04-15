dbf2f964819baad27d027af72633728eb7098805
Alvaro <aldc30sc@gmail.com>
2022-04-15 12:40:52 +0200

    test: :white_check_mark: Add test for GET_USERS returning an error

--- apps/user-list-redux/src/app/actions/users.spec.ts
+++ apps/user-list-redux/src/app/actions/users.spec.ts
@@ -34,5 +34,26 @@ describe('User actions', () => {
             expect(actions[1].type).toEqual('GET_USERS_FULFILLED');
             expect(actions[1].payload.data[0].name).toEqual('Vasilis');
         });
+
+        it('dispatches GET_USERS action and returns an error', async () => {
+            mockedAxios.get.mockImplementationOnce(() =>
+                Promise.reject({
+                    error: 'Something bad happened',
+                })
+            );
+
+            try {
+                await store.dispatch(getUsers());
+            } catch (e) {
+                const actions = store.getActions();
+
+                expect.assertions(3);
+                expect(actions[0].type).toEqual('GET_USERS_PENDING');
+                expect(actions[1].type).toEqual('GET_USERS_REJECTED');
+                expect(actions[1].payload.error).toEqual(
+                    'Something bad happened'
+                );
+            }
+        });
     });
 });
