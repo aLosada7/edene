023e130bbb26f68427f87f2f921e222a74529acc
Alvaro <aldc30sc@gmail.com>
2022-04-15 19:13:52 +0200

    feat: :sparkles: Configure redux store

--- apps/user-list-redux/src/main.tsx
+++ apps/user-list-redux/src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from './app/app';
+import configureStore from './store';
 
 ReactDOM.render(
     <StrictMode>
-        <App />
+        <Provider store={configureStore()}>
+            <App />
+        </Provider>
     </StrictMode>,
     document.getElementById('root')
 );
