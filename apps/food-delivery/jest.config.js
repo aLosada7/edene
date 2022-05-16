9dda49d4e01f76c2a6d0ae10886923f437c65890
Alvaro <aldc30sc@gmail.com>
2022-05-16 18:47:57 +0200

    feat: :sparkles: Configure react-testing-library in food-delivery app

--- apps/food-delivery/jest.config.js
+++ apps/food-delivery/jest.config.js
@@ -7,4 +7,5 @@ module.exports = {
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     coverageDirectory: '../../coverage/apps/food-delivery',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
 };
