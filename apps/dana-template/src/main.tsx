4ab29446b36aba8f7e0f602b6918d10bd27ba8fd
Alvaro <aldc30sc@gmail.com>
2022-06-10 09:31:36 +0200

    chore: :arrow_up: Upgrade Nx version to 14.2.4

--- apps/dana-template/src/main.tsx
+++ apps/dana-template/src/main.tsx
@@ -1,14 +1,16 @@
 import { DanaTheme } from '@dana-theme';
 import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 
 import { Layout, Router } from './shared/hoc';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
     <StrictMode>
         <DanaTheme>
             <Router />
         </DanaTheme>
-    </StrictMode>,
-    document.getElementById('root')
+    </StrictMode>
 );
--- libs/dana-labs/jest.config.ts
+++ libs/dana-labs/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
     displayName: 'dana-labs',
     preset: '../../jest.preset.js',
     transform: {
