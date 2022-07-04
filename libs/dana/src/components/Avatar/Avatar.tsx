8bebc41013c218fdc1033ed1d86b69110d0c111e
Alvaro <aldc30sc@gmail.com>
2022-07-04 19:49:46 +0200

    refactor: :recycle: Avatar component refactor (scss to emotion)

--- libs/dana/src/components/Avatar/Avatar.tsx
+++ libs/dana/src/components/Avatar/Avatar.tsx
@@ -1,67 +1,66 @@
 import { useState, useEffect } from 'react';
-import './avatar.scss';
+
+import { useThemeContext } from '@dana-theme';
 
 import defaultAvatar from '../../../assets/img/avatarIcon.jpeg';
+import { avatar, avatarConnected, avatarImg, avatarInitials } from './styles';
 
 export type AvatarShape = 'circle' | 'square';
 export type AvatarDotPosition = 'top' | 'bottom';
 
 export interface AvatarProps {
-  src?: string;
-  name?: string;
-  shape?: AvatarShape;
-  connected?: boolean;
-  outline?: boolean;
-  dotPosition?: AvatarDotPosition;
+    src?: string;
+    name?: string;
+    shape?: AvatarShape;
+    connected?: boolean;
+    outline?: boolean;
+    dotPosition?: AvatarDotPosition;
 }
 
 export function Avatar({
-  shape = 'circle',
-  dotPosition = 'top',
-  ...props
+    shape = 'circle',
+    dotPosition = 'top',
+    connected,
+    outline,
+    ...props
 }: AvatarProps) {
-  const [initials, setInitials] = useState('');
-  useEffect(() => {
-    if (props.name)
-      setInitials(
-        props.name
-          .split(' ')
-          .map((initial) => initial[0])
-          .join('')
-      );
-  }, [props.name]);
-  return (
-    <div className={`avatar ${shape === 'circle' ? 'circle' : 'square'}`}>
-      {props.src ? (
-        <img
-          src={props.src}
-          className={`img ${shape === 'circle' ? 'circle' : 'square'} ${
-            props.outline ? 'outline' : ''
-          }`}
-          alt="Avatar"
-        ></img>
-      ) : props.name ? (
-        <div className="img circle flex-center initials">
-          <span>{initials}</span>
+    const [initials, setInitials] = useState<string | undefined>();
+    useEffect(() => {
+        if (props.name)
+            setInitials(
+                props.name
+                    .split(' ')
+                    .map((initial) => initial[0])
+                    .join('')
+            );
+    }, [props.name]);
+
+    const { theme } = useThemeContext();
+
+    return (
+        <div css={avatar(shape, { theme })}>
+            {props.src ? (
+                <img
+                    src={props.src}
+                    css={avatarImg(shape, outline)}
+                    alt="Avatar"
+                ></img>
+            ) : props.name ? (
+                <div css={[avatarImg('circle'), avatarInitials({ theme })]}>
+                    <span>{initials}</span>
+                </div>
+            ) : (
+                <img
+                    src={defaultAvatar}
+                    css={avatarImg('circle')}
+                    alt="Default Avatar"
+                ></img>
+            )}
+            {connected ? (
+                <span css={avatarConnected(dotPosition, shape)}></span>
+            ) : null}
         </div>
-      ) : (
-        <img
-          src={defaultAvatar}
-          className="img circle icon"
-          alt="Default Avatar"
-        ></img>
-      )}
-      {props.connected ? (
-        <span
-          className={`connected ${
-            shape === 'circle'
-              ? `${dotPosition}-circle`
-              : `${dotPosition}-square`
-          }`}
-        ></span>
-      ) : null}
-    </div>
-  );
+    );
 }
 
 export default Avatar;
---
+++ libs/dana/src/components/Avatar/styles.ts
@@ -0,0 +1,78 @@
+import { css } from '@emotion/react';
+
+import { defaultTheme } from '@dana-theme';
+
+import { AvatarShape, AvatarDotPosition } from './Avatar';
+
+export const avatar = (shape: AvatarShape, { theme = defaultTheme }) => css`
+    position: relative;
+    max-width: 12rem;
+    width: 80px;
+    height: 80px;
+    cursor: pointer;
+    font-size: 6rem;
+    background-color: ${theme.black};
+    color: ${theme.white};
+
+    ${avatarShape(shape)}
+`;
+
+export const avatarImg = (shape: AvatarShape, outline?: boolean) => css`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    position: absolute;
+
+    ${avatarShape(shape)}
+    ${outline && `outline: thick double #32a1ce;`}
+`;
+
+const avatarShape = (shape: AvatarShape) => css`
+    border-radius: ${shape === 'circle' ? '50%' : '14px'};
+`;
+
+export const avatarConnected = (
+    dotPosition: AvatarDotPosition,
+    shape: AvatarShape
+) => css`
+    display: block;
+    position: absolute;
+    width: 1.5rem;
+    height: 1.5rem;
+    border-radius: 50%;
+    border: 2px solid #ffffff;
+    background: green;
+
+    ${dotPosition === 'top'} {
+        ${shape === 'square'} {
+            top: -0.25rem;
+            right: -0.25rem;
+        }
+
+        ${shape === 'circle'} {
+            top: 1rem;
+            right: 1rem;
+        }
+    }
+
+    ${dotPosition === 'bottom'} {
+        ${shape === 'square'} {
+            bottom: -0.25rem;
+            right: -0.25rem;
+        }
+
+        ${shape === 'circle'} {
+            bottom: 1rem;
+            right: 1rem;
+        }
+    }
+`;
+
+export const avatarInitials = ({ theme = defaultTheme }) => css`
+    background-color: ${theme.black};
+    color: ${theme.white};
+    font-size: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
