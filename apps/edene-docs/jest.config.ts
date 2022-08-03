9b297317c376b3a7d91a186a5e8c38d21b9912cb
Alvaro <aldc30sc@gmail.com>
2022-08-03 19:52:52 +0200

    chore: :truck: Dana-docs is now edene-docs

--- apps/edene-docs/jest.config.ts
+++ apps/edene-docs/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-    displayName: 'dana-docs',
+    displayName: 'edene-docs',
     preset: '../../jest.preset.js',
     transform: {
         '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
         '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    coverageDirectory: '../../coverage/apps/dana-docs',
+    coverageDirectory: '../../coverage/apps/edene-docs',
 };
