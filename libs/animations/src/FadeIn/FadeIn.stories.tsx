c485e3a315c001c3b02f04504c76213abe5e3d87
Alvaro <aldc30sc@gmail.com>
2022-11-15 22:24:59 +0100

    refactor: :recycle: remove title from story

--- libs/animations/src/FadeIn/FadeIn.stories.tsx
+++ libs/animations/src/FadeIn/FadeIn.stories.tsx
@@ -1,6 +1,8 @@
 import { Story } from '@storybook/react';
 
-import { FadeIn } from './FadeIn';
+import { Text } from '@edene/components';
+
+import { FadeIn } from './index';
 
 export default {
     title: 'Animations',
@@ -9,25 +11,22 @@ export default {
 
 export const Default: Story = () => {
     return (
-        <>
-            <h3>FadeIn Transition</h3>
-            <FadeIn delay="1s">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Vivamus id rutrum orci. Sed pulvinar aliquet nulla, sed
-                    lacinia dui egestas id. Donec lorem justo, dictum at
-                    malesuada a, condimentum ac libero. Aenean sollicitudin
-                    dictum velit, eu congue urna malesuada eget. Maecenas tempor
-                    tempor massa id rutrum. Nam nec quam blandit, blandit arcu
-                    ac, lacinia tortor. Class aptent taciti sociosqu ad litora
-                    torquent per conubia nostra, per inceptos himenaeos. Integer
-                    blandit mollis auctor. Nullam in massa quis felis fermentum
-                    iaculis non a risus. Vivamus a lorem nec tortor posuere
-                    egestas. Proin varius imperdiet nibh vestibulum efficitur.
-                    Duis ac urna eu leo vehicula rhoncus id id odio.
-                </p>
-            </FadeIn>
-        </>
+        <FadeIn delay="1s">
+            <Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                id rutrum orci. Sed pulvinar aliquet nulla, sed lacinia dui
+                egestas id. Donec lorem justo, dictum at malesuada a,
+                condimentum ac libero. Aenean sollicitudin dictum velit, eu
+                congue urna malesuada eget. Maecenas tempor tempor massa id
+                rutrum. Nam nec quam blandit, blandit arcu ac, lacinia tortor.
+                Class aptent taciti sociosqu ad litora torquent per conubia
+                nostra, per inceptos himenaeos. Integer blandit mollis auctor.
+                Nullam in massa quis felis fermentum iaculis non a risus.
+                Vivamus a lorem nec tortor posuere egestas. Proin varius
+                imperdiet nibh vestibulum efficitur. Duis ac urna eu leo
+                vehicula rhoncus id id odio.
+            </Text>
+        </FadeIn>
     );
 };
 Default.storyName = 'FadeIn';
