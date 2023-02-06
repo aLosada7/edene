9a99b081f2ec2b7eafdb49ed6f8b99c70832916a
aLosada7 <aldc30sc@gmail.com>
2023-02-06 18:05:46 +0100

    feat: scales from foundations

--- libs/components/src/layout/Box/types.ts
+++ libs/components/src/layout/Box/types.ts
@@ -1,13 +1,21 @@
-import { PolymorphicComponentProps, Props } from '@edene/foundations';
+import { PolymorphicComponentProps, Props, Scale } from '@edene/foundations';
 import { ReactNode } from 'react';
 
 export interface BoxProps extends Props {
-    mt?: number;
-    mr?: number;
-    mb?: number;
-    ml?: number;
-    mv?: number;
-    mh?: number;
+    m?: Scale;
+    mt?: Scale;
+    mr?: Scale;
+    mb?: Scale;
+    ml?: Scale;
+    my?: Scale;
+    mx?: Scale;
+    p?: Scale;
+    pt?: Scale;
+    pr?: Scale;
+    pb?: Scale;
+    pl?: Scale;
+    py?: Scale;
+    px?: Scale;
     children: ReactNode;
 }
 
